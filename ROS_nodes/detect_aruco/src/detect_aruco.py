b57b5941a620224d7027af1effa3fa5291012826
andkgl <angle14@student.sdu.dk>
2018-05-08 19:36:56 +0200

    Aruco detector up and running. Sends info about the marker detected: X, Y, rotation_1, rotation_2, distance, marker_found. Marker_found == 1 if found.

--- ROS_nodes/detect_aruco/src/detect_aruco.py
+++ ROS_nodes/detect_aruco/src/detect_aruco.py
@@ -9,24 +9,78 @@ from cv_bridge import CvBridge
 from std_msgs.msg import Int32MultiArray
 # Import other libraries
 import cv2
-import numpy
+import numpy as np
+
+import cv2.aruco as aruco
+import yaml
+import math
 
 # Initialize static content
 bridge = CvBridge()
+
+# https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+def rotationMatrixToEulerAngles(R):
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
 
 def main():
     rospy.init_node('Detect_ArUco',anonymous=False)
     img_sub = rospy.Subscriber('camera/raw',Image,callback,queue_size=1)
 
 """
-Insert your image detection code in this function Anders, and publish in a Int32MultiArry type
+Insert your image detection code in this function Anders, and publish in a Int32MultiArray type
 """
 def callback(img):
-    print('recieved data')
+    print('Recieved data')
 
-    Int32MultiArray my_array_for_publishing[0]=1
-    
-    pub( my_array_for_publishing)
+    with open("calibration2.yaml") as f:
+        loadeddict = yaml.load(f)
+        camera_matrix = np.asarray(loadeddict.get("camera_matrix"))
+        dist_coeffs = np.asarray(loadeddict.get("dist_coeff"))
+
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_1000)
+    parameters = aruco.DetectorParameters_create()
+
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+
+    # img = aruco.drawDetectedMarkers(img, corners)
+
+    accepted = 0
+    if ids != None:
+        accepted = 1
+        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 1000, camera_matrix, dist_coeffs)
+
+
+        for i in range (len(rvec)):
+            # gray = aruco.drawAxis(gray, camera_matrix, dist_coeffs, rvec[i], tvec[i], 200)
+
+            r = cv2.Rodrigues(rvec[i])
+            angles = rotationMatrixToEulerAngles(r[0])
+            angles = angles / 3.14 * 180
+
+            rotation1 = angles[2]
+            rotation2 = angles[1]
+            mid = (corners[0][0][0] + corners[0][0][2]) / 2
+            dist = tvec[i][2]
+
+            mydata = [mid[0], mid[1], rotation1, rotation2, dist, accepted]
+            myPubArray = Float64MultiArray(data=mydata)
+            pub(myPubArray)
+
+	
 
 def pub(data):
     aruco_pub = rospy.Publisher('ArUco/data_array', Int32MultiArray, queue_size=1)
